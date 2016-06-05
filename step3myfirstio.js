df8ca27192ec1b3ef886bb5afbaf3ee7961f988d
Azurasky <tarutaruwhm@live.com>
2016-06-05 01:41:58 -0400

    Finished step 3 with comments

--- step3myfirstio.js
+++ step3myfirstio.js
@@ -11,8 +11,29 @@
  */
  
  var fs = require('fs');
- var file = process.argv[2];
- console.log(file.split("\n").length));
+ var file = fs.readFileSync(process.argv[2]);
+ file = file.toString();
+ console.log(file.split("\n").length - 1);
+ 
+     // note you can avoid the .toString() by passing 'utf8' as the
+    // second argument to readFileSync, then you'll get a String!
+    //
+    // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
+	
+	  /* If you're looking for an easy way to count the number of newlines in a
+  string, recall that a JavaScript String can be .split() into an array of
+  substrings and that '\n' can be used as a delimiter. Note that the test
+  file does not have a newline character ('\n') at the end of the last line,
+  so using this method you'll end up with an array that has one more element
+  than the number of newlines.
+  
+  */
+  
+  // notes: pretty straightforward
+  // fs = file system module
+  // buffer object is node's way of representing arrays of data in many formats
+  // toString() converts buffers to strings
+
  
  
  
